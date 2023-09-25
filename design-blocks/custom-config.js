dced52d89f76155f0e6754d4293914f9e4b95431
kaychanela <kaychanela@gmail.com>
2023-09-24 22:03:29 -0700

    Updated design2.md

--- design-blocks/custom-config.js
+++ design-blocks/custom-config.js
@@ -1,18 +1,20 @@
-// Initialize the workspace
-var workspace = Blockly.inject('blocklyDiv', {
-    toolbox: document.getElementById('toolbox'),
-    // Other configuration options here
+// This function initializes the workspace and sets up the toolbox
+document.addEventListener("DOMContentLoaded", function () {
+    var workspace = Blockly.inject('blocklyDiv', {
+        toolbox: document.getElementById('toolbox'),
+        // Other configuration options here
+    });
+
+    // Define your toolbox blocks (you can customize this as needed)
+    var toolboxXML = `
+    <xml id="toolbox" style="display: none">
+        <category name="Basic">
+            <block type="text"></block>
+            <!-- Add more blocks here -->
+        </category>
+    </xml>
+    `;
+
+    // Set the toolbox
+    workspace.updateToolbox(toolboxXML);
 });
-
-// Define your toolbox blocks (you can customize this as needed)
-var toolboxXML = `
-<xml id="toolbox" style="display: none">
-    <category name="Basic">
-        <block type="text"></block>
-        <!-- Add more blocks here -->
-    </category>
-</xml>
-`;
-
-// Set the toolbox
-workspace.updateToolbox(toolboxXML);
